95203cddf7c783c2c3369f4c53249572389067a1
Sarhan Alim <sarhan.alim@gmail.com>
2023-04-23 12:24:08 +0100

    variables, reassigning and incrementing, math operations & console log

--- 1. Build a Passenger Counter App/02. Write your first JavaScript variable/index.js
+++ 1. Build a Passenger Counter App/02. Write your first JavaScript variable/index.js
@@ -1,3 +1,11 @@
+// document.getElementById("count-el").innerText = 5
+
+let count = 0
+
+console.log(count)
+
 // 1. Create a variable, myAge, and set its value to your age
+let myAge = 37
 
 // 2. Log the myAge variable to the console
+console.log(myAge)
---
+++ 1. Build a Passenger Counter App/03. Basic mathematical operations/index.js
@@ -0,0 +1,16 @@
+// document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 9
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+// 1. Create two variables, myAge and humanDogRatio
+let myAge = 37
+let humanDogRatio = 7
+// 2. Multiply the two together and store the result in myDogAge
+let myDogAge = myAge * humanDogRatio
+// 3. Log myDogAge to the console (1 human year = 7 dog years)
+console.log(myDogAge)
