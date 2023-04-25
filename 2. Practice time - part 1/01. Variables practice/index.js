28a1e015c94b1f15bed43177d3b2653afb333c9e
Sarhan Alim <sarhan.alim@gmail.com>
2023-04-25 23:17:02 +0100

    variables, numbers and strings, functions, DOM recap  challenges

--- 2. Practice time - part 1/01. Variables practice/index.js
+++ 2. Practice time - part 1/01. Variables practice/index.js
@@ -1,7 +1,9 @@
 // Create two variables, firstName and lastName
+let firstName = "Bruce";
+let lastName = "Lee";
 
 // Concatenate the two variables into a third variable called fullName
+fullName = firstName + " " + lastName;
 
 // Log fullName to the console
-
-
+console.log(fullName);
--- 2. Practice time - part 1/05. Rendering an error message/index.js
+++ 2. Practice time - part 1/05. Rendering an error message/index.js
@@ -2,4 +2,8 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+let errorParagraph = document.getElementById("error");
 
+function purchase() {
+  errorParagraph.textContent = "Something went wrong, please try again"
+}
