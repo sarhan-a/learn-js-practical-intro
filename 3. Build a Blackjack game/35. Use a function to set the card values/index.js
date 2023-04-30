660cdcda11e05d6a88a735aa508139a8fd34b2a2
Sarhan Alim <sarhan.alim@gmail.com>
2023-04-30 12:26:58 +0100

    returning values in functions & hoisting. Using a function to set the card values

--- 3. Build a Blackjack game/35. Use a function to set the card values/index.js
+++ 3. Build a Blackjack game/35. Use a function to set the card values/index.js
@@ -1,4 +1,49 @@
 // 2. Use getRandomCard() to set the values of firstCard and secondCard
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 // 1. Create a function, getRandomCard(), that always returns the number 5
+function getRandomCard() {
+  return 5;
+}
 
+function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: "
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
+
+  if (sum <= 20) {
+    message = "Do you want to draw another card?";
+  } else if (sum === 21) {
+    message = "You got BlackJack!";
+    hasBlackJack = true;
+  } else {
+    message = "Busted! Sorry, you're out";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = getRandomCard();
+  cards.push(card);
+  console.log(cards);
+  sum += card;
+  renderGame();
+}
