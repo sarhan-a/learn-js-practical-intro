78ca3094b7a4a3a733f02733ea5899f5a44680ea
Sarhan Alim <sarhan.alim@gmail.com>
2023-04-30 15:02:23 +0100

    logical AND / OR - New Card button to work only if alive and no blackjack

---
+++ 3. Build a Blackjack game/45. The logical AND operator/index.js
@@ -0,0 +1,10 @@
+let hasCompletedCourse = true;
+let givesCertificate = true;
+
+if (hasCompletedCourse && givesCertificate) {
+  generateCertificate();
+}
+
+function generateCertificate() {
+  console.log("Generating certificate...")
+}
