61eac66b201f12c126f73243461d5754d3ac6d52
Sarhan Alim <sarhan.alim@gmail.com>
2023-05-02 18:11:02 +0100

    add & style input and button, use addEventListener

--- 5. Build a Chrome Extension/06. Write your first addEventListener()/index.js
+++ 5. Build a Chrome Extension/06. Write your first addEventListener()/index.js
@@ -1,3 +1,7 @@
 // 1. Grab the box from the DOM and store it in a variable
-// 2. Add a click event listener to the box 
+let openBox = document.getElementById("box");
+// 2. Add a click event listener to the box
 // 3. Log out "I want to open the box!" when it's clicked
+openBox.addEventListener("click", function () {
+  console.log("I want to open the box!");
+})
--- 5. Build a Chrome Extension/07. Your turn to refactor/index.js
+++ 5. Build a Chrome Extension/07. Your turn to refactor/index.js
@@ -1,3 +1,8 @@
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 
+let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function () {
+  console.log("Button Clicked!")
+})
