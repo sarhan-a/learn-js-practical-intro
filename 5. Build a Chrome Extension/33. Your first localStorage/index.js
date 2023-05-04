952024b69ad2b5c884ef6a604e6e2d84c201b54e
Sarhan Alim <sarhan.alim@gmail.com>
2023-05-04 23:58:43 +0100

    deployed unpacked, learn to use localStorage

--- 5. Build a Chrome Extension/33. Your first localStorage/index.js
+++ 5. Build a Chrome Extension/33. Your first localStorage/index.js
@@ -1,3 +1,7 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 // 1. Save a key-value pair in localStorage
 // 2. Refresh the page. Get the value and log it to the console
@@ -8,3 +12,31 @@
 // localStorage.getItem(key)
 // localStorage.clear()
 // PS: both key and value need to be strings
+
+localStorage.setItem("france", "Paris")
+
+let capital = localStorage.getItem("france")
+console.log(capital);
+localStorage.clear()
+console.log(capital);
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = ""
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li>
+      <a target='_blank' href='${myLeads[i]}'>
+        ${myLeads[i]}
+      </a>
+    </li>
+  `
+    console.log(listItems)
+  }
+  ulEl.innerHTML = listItems;
+}
