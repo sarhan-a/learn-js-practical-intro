f5bd93052869e196fa1dee0b77a2ea47cc8c1eb8
Sarhan Alim <sarhan.alim@gmail.com>
2023-05-07 23:55:51 +0100

    functions with strings, numbers, arrays as param, arguments vs params

--- 5. Build a Chrome Extension/44. Functions with multiple parameters/index.js
+++ 5. Build a Chrome Extension/44. Functions with multiple parameters/index.js
@@ -1,2 +1,8 @@
+const welcomeEl = document.getElementById("welcome-el")
 
-    // Rewrite the expression using template literals
+// Rewrite the expression using template literals
+function greetUser(greeting, name, emoji) {
+    welcomeEl.textContent = `${greeting}, ${name} ${emoji}`
+}
+
+greetUser("Good Morning", "John", "😁")
